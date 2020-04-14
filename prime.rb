0859208d37c76b1fc4ad9ab2426ef3622dcc7fae
global-event-2762 <rosalinschlosser@gmail.com>
2020-04-14 23:07:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(int)
+  if int.abs == 1 || int == 0
+    return false;
+  end
+  (2..(int.abs-1)).each do |x|
+    if int % x == 0
+      return false;
+    end
+  end
+  true;
+end
