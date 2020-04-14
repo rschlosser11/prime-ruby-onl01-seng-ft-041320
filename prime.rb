029e85f765af102c3515d9943f6df473a3e7a49f
global-event-2762 <rosalinschlosser@gmail.com>
2020-04-14 22:55:13 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,6 +3,8 @@ def prime?(int)
   dividable_by = numbers.select {|num| int % num == 0}
   if (int <= 9 && dividable_by.length < 2 && dividable_by.any?(int) && int > 0) || dividable_by.length < 1
     true
+  elsif int == 1 || int == -1
+    false
   else
     false
   end
