549b92454cefe71284ba561f1f4adb1fc44e7912
global-event-2762 <rosalinschlosser@gmail.com>
2020-04-14 22:55:43 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 def prime?(int)
   numbers = (2..9).to_a
   dividable_by = numbers.select {|num| int % num == 0}
-  if (int <= 9 && dividable_by.length < 2 && dividable_by.any?(int) && int > 0) || dividable_by.length < 1
-    true
-  elsif int == 1 || int == -1
+  if int == 1 || int == -1
+    false
+  elsif (int <= 9 && dividable_by.length < 2 && dividable_by.any?(int) && int > 0) || dividable_by.length < 1
     false
   else
     false
